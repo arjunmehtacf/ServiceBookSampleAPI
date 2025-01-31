9d8dd4cf59d4609e1cce321a353e603851d6ab33
arjunmehtaspec <117633064+arjunmehtaspec@users.noreply.github.com>
2025-01-31 17:16:26 +0530

    Changes for railway server deployment

--- config/db.js
+++ config/db.js
@@ -1,13 +1,19 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 // Database connection
 const db = mysql.createConnection({
   // host: 'servicbook-database.cbk4ossa6bpb.us-east-1.rds.amazonaws.com',
-  host: 'localhost',
+  // host: 'localhost',
+  //mysql://root:kfhuOThTptyOluLGLqlMFXrHaFGavoBe@roundhouse.proxy.rlwy.net:15303/railway
+  host:'roundhouse.proxy.rlwy.net',
   user: 'root',
-  password:'root123',
+  password: 'kfhuOThTptyOluLGLqlMFXrHaFGavoBe',
+  // password:'root123',
   // password: 'Dhruv$100690',
-  database: 'compose'
+  // database: 'compose'
+  database: 'railway',
+  port: 15303, // Use the Railway port from your credentials
+  connectTimeout: 10000,
 });
 
 db.connect((err) => {
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
 
-const PORT = 3000;
+const PORT = 15303;
 
 // Start the server
 app.listen(PORT, () => {
